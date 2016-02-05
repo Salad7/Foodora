f50d04398fe95a6315e094f9e953976813f884f2
Mohamed Salad <msalad@uncc.edu>
2016-02-05 18:57:47 -0500

    Menu update
    Updated Menu Screen to include ArcLayout.
    
    Credits to  ogaclejapan for design and creation.

--- app/src/main/java/com/example/salad/foodora/Home.java
+++ app/src/main/java/com/example/salad/foodora/Home.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +20,14 @@ public class Home extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_local);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
