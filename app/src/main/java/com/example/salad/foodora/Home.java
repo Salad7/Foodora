9029bdad71edaee91652e6f963bdfdd2079d4b1e
Mohamed Salad <msalad@uncc.edu>
2016-02-05 19:08:37 -0500

    Updated home page
     credit to ogaclejapan

--- app/src/main/java/com/example/salad/foodora/Home.java
+++ app/src/main/java/com/example/salad/foodora/Home.java
@@ -30,6 +30,8 @@ public class Home extends AppCompatActivity {
 //        });
     }
 
+    //Test
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
